e5e1d89260a7ce73c5ead7f2e2411edce6999c6f
Susanto Sarker <susantosarkermail@gmail.com>
2026-09-09 02:15:20 +0600

    Question 3: Username Gatekeeper

--- answar.js
+++ answar.js
@@ -11,7 +11,8 @@ function describeValue(value) {
 // Question 2: Bangladesh Weekend Machine
 
 function getDayType(value) {
-  const capitalizedValue = value.charAt(0).toUpperCase() + value.slice(1);
+  const val = value.toLowerCase();
+  const capitalizedValue = val.charAt(0).toUpperCase() + val.slice(1);
 
   switch (capitalizedValue) {
     case "Friday":
@@ -28,4 +29,19 @@ function getDayType(value) {
   }
 }
 
+// Question 3: Username Gatekeeper
+
+function validateUsername(pass) {
+  const lowPass = pass.toLowerCase();
+
+  if (lowPass.length < 4) {
+    return "Too Short";
+  } else if (lowPass.includes(" ")) {
+    return "No Space Allowed";
+  } else if (lowPass.includes("admin")) {
+    return "Reserved Word";
+  } else {
+    return "Available";
+  }
+}
 
