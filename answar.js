21460c5dc35d578a37af77f674a61b21476c521d
Susanto Sarker <susantosarkermail@gmail.com>
2026-09-09 01:53:54 +0600

    Question 2: Bangladesh Weekend Machine

--- answar.js
+++ answar.js
@@ -1,10 +1,31 @@
 // Question 1: Value Detective
 
 function describeValue(value) {
-   const typeofValue = typeof value
+  const typeofValue = typeof value;
 
-   const val = value ? "truthy" : "falsy";
+  const val = value ? "truthy" : "falsy";
 
-   return `${typeofValue} | ${val}`
+  return `${typeofValue} | ${val}`;
 }
+
+// Question 2: Bangladesh Weekend Machine
+
+function getDayType(value) {
+  const capitalizedValue = value.charAt(0).toUpperCase() + value.slice(1);
+
+  switch (capitalizedValue) {
+    case "Friday":
+    case "Saturday":
+      return "Weekend";
+    case "Sunday":
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+      return "Working Day";
+    default:
+      return "Invalid Day";
+  }
+}
+
 
